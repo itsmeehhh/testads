4e6ef287c5540df6f91a709fef055041b71b9c3b
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-13 16:35:39 +0000

    .

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/test/index.html');
 });
 
-app.listen(8080, () => {
-  console.log('Server listening on port 3000');
+app.listen(port, () => {
+  console.log('Server listening on port ${port}');
 });
